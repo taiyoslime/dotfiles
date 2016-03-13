d22f6e9141ff2521fe99e21b3d4ea40db2744425
taiyoslime <hase.tekitou@gmail.com>
2016-03-13 20:01:55 +0900

    some changes

--- zsh/.tszsh/.tsalias.zsh
+++ zsh/.tszsh/.tsalias.zsh
@@ -13,7 +13,12 @@ alias ...="cd ../.."
 # related 'extract' function
 alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
 
+# youtube-dl utility
+alias ydl="youtube-dl -f 'bestvideo[ext!=webm]'"
+alias ydl-audio="youtube-dl -f 'bestaudio[ext!=webm]' --audio-quality 0"
+alias ydl-list="youtube-dl -F"
+
 # git utility
-alias gp="git push origin master"
+alias gp="git push -u origin master"
 alias gc="git commit -a -m"
 alias gs="git status"
