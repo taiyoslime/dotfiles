4b348eaf20669fa7acfc3a20ff68834c8626f8a4
taiyoslime <hase.tekitou@gmail.com>
2016-08-26 19:39:35 +0900

    checkout

--- zsh/tszsh/.tsinit.zsh
+++ zsh/tszsh/.tsinit.zsh
@@ -1,5 +1,6 @@
 # source all scripts
-source "${TAIYOSLIME_SHDIR}/.tszsh/.tsenv.zsh"
+source "${TAIYOSLIME_SHDIR}
+/.tszsh/.tsenv.zsh"
 source "${TAIYOSLIME_SHDIR}/.tszsh/.tsalias.zsh"
 source "${TAIYOSLIME_SHDIR}/.tszsh/.tsfunc.zsh"
 source "${TAIYOSLIME_SHDIR}/.tszsh/.tszstyle.zsh"
