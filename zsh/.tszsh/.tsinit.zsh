6d80316b7a4f9552c3a58b6f0b722aa63b6de6a0
taiyoslime <hase.tekitou@gmail.com>
2016-08-26 19:12:08 +0900

    to rebuild shell-script's manage system

--- zsh/.tszsh/.tsinit.zsh
+++
@@ -1,5 +0,0 @@
-# source all scripts
-source "${TAIYOSLIME_SHDIR}/.tszsh/.tsenv.zsh"
-source "${TAIYOSLIME_SHDIR}/.tszsh/.tsalias.zsh"
-source "${TAIYOSLIME_SHDIR}/.tszsh/.tsfunc.zsh"
-source "${TAIYOSLIME_SHDIR}/.tszsh/.tszstyle.zsh"
--- zsh/.tszsh/.tszstyle.zsh
+++
@@ -1,2 +0,0 @@
-# ps コマンドのプロセス名補完
-zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
