50782552590e583fd5d8647289f4a176c31c8b1e
taiyoslime <hase.tekitou@gmail.com>
2016-12-29 16:22:46 +0900

    add and change files(no comment)

--- zsh/.tszsh/.tszstyle.zsh
+++ zsh/.tszsh/.tszstyle.zsh
@@ -1,2 +1,11 @@
 # ps コマンドのプロセス名補完
 zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
+
+zstyle ':prezto:module:syntax-highlighting' color 'yes'
+
+zstyle ':prezto:module:syntax-highlighting' highlighters \
+  'main' \
+  'brackets' \
+  'pattern' \
+  'cursor' \
+  'root' 
