14122b6549a140b093898ce999594acdf7a731e2
taiyoslime <t@iyosli.me>
2020-04-16 02:23:17 +0900

    exclude npm

--- npm/README.md
+++
@@ -1,5 +0,0 @@
-
-|name||
-|:--|:--|
-|install.sh|to install npm modules|
-|package.json|configration for npm|
--- npm/_install.sh
+++
@@ -1,6 +0,0 @@
-# must run this script after run ../brew/install.sh
-
-sudo ln -sf /usr/local/lib/package.json ./package.json
-
-# install modules using package.json
-sudo npm install -g
